284a9fe57e76cc2254837691315b8b8e099a4385
nluu175 <nluu@ualberta.ca>
2022-01-21 11:54:39 -0700

    added events; added README.md

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { token } = config;
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+// Registering Commands
 client.commands = new Collection();
 const commandFiles = fs
   .readdirSync("./commands")
@@ -16,26 +17,18 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
-client.once("ready", () => {
-  console.log("Ready!");
-});
+// Registering Events
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    return interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
+}
 
 client.login(token);
